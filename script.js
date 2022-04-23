b74f89dbe618d5a55d7ac07fc309beac9eb11876
Sheldon Gelinas <sheldoniveygelinas1234@gmail.com>
2022-04-23 10:54:23 -0400

    master

--- script.js
+++
@@ -1,8 +0,0 @@
-fetch('https://api.imgflip.com/get_memes')
-.then(response => response.json())
-.then(data => {
-    var memes = data.data.memes;
-    for(i = 0; i < memes.length; i++){
-        document.getElementById('singles').insertAdjacentHTML('afterBegin', `<a href='${memes[i].url}'><img class='singleImg' src='${memes[i].url}'></a>`)
-    }
-})
