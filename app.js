f7d5dbb78e4940b61176c8a7854f59c040c166b6
Sheldon Gelinas <sheldoniveygelinas1234@gmail.com>
2022-04-26 14:34:55 -0400

    message

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.set('view engine','ejs');
 app.engine('ejs', require('ejs').__express);
 
 app.get('/', function (req, res) {
-
     res.render(__dirname + '/index'); 
-
-
 });
+app.get('/news', function (req, res) {
+        res.render(__dirname + '/news'); 
+    });
+
 
 
 app.listen(3000);
